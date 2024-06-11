825e4cd33224ae579d8362c78dd411d05ab9e436
r1yk <49421368+r1yk@users.noreply.github.com>
2024-06-11 15:48:00 -0400

    find submitted answers, handle events between windows

---
+++ scripts/lib.js
@@ -0,0 +1,13 @@
+// Functions to be used within the application
+
+export function getAnswers() {
+  return [];
+}
+
+function getSubmitted() {
+  return [];
+}
+
+function getMissing() {
+  return [];
+}
---
+++ scripts/load.js
@@ -0,0 +1,34 @@
+// This runs when the page load first happens.
+let gameData = {};
+
+function getAnswers() {
+  if (gameData?.today) {
+    return gameData.today.answers;
+  }
+}
+
+function getSubmitted() {
+  elements = document.querySelectorAll(".sb-wordlist-pag span");
+  return Array.from(elements).map((element) => element.innerText.toLowerCase());
+}
+
+function getMissing() {
+  submitted = getSubmitted();
+  return getAnswers.filter((answer) => !submitted.includes(answer));
+}
+
+window.addEventListener(
+  "message",
+  (event) => {
+    // We only accept messages from ourselves
+    if (event.source !== window) {
+      return;
+    }
+
+    messageType = event?.data?.type;
+    if (messageType === "GAME_DATA") {
+      gameData = JSON.parse(event.data.message);
+    }
+  },
+  false
+);
