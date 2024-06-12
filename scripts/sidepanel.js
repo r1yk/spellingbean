c91e49fbe5ee536ffd1bbba8c18832dd9083ea5e
r1yk <49421368+r1yk@users.noreply.github.com>
2024-06-12 11:12:05 -0400

    get tabs working within sidepanel

--- scripts/sidepanel.js
+++ scripts/sidepanel.js
@@ -1,24 +1,44 @@
 console.log("SIDE PANEL JS");
 
-async function displayAnswers() {
+async function renderHintsAndAnswers() {
   data = await chrome.storage.session.get([
     "spellingBeanAnswers",
     "spellingBeanSubmitted",
   ]);
   const { spellingBeanAnswers, spellingBeanSubmitted } = data;
 
+  renderHints(spellingBeanAnswers, spellingBeanSubmitted);
+  renderAnswers(spellingBeanAnswers, spellingBeanSubmitted);
+}
+
+function renderHints(answers, submitted) {}
+
+function renderAnswers(answers, submitted) {
   const answersTableMissing = document.getElementById("answers-table-missing");
   const answersTable = document.getElementById("answers-table");
 
   rowsMissing = [];
   rowsAll = [];
-  spellingBeanAnswers.forEach((answer) => {
+  answers.forEach((answer) => {
     row = document.createElement("tr");
     answerCell = document.createElement("td");
     answerCell.innerText = answer;
-    row.append(answerCell);
 
-    if (!spellingBeanSubmitted.includes(answer)) {
+    definitionCell = document.createElement("td");
+    definitionLink = document.createElement("a");
+    definitionLink.innerText = "(definition)";
+    definitionLink.setAttribute(
+      "href",
+      `https://www.oed.com/search/dictionary/?scope=Entries&q=${answer}`
+    );
+    definitionLink.setAttribute("target", "_blank");
+    definitionLink.setAttribute("rel", "noopener noreferrer");
+    definitionCell.append(definitionLink);
+
+    row.append(answerCell);
+    row.append(definitionCell);
+
+    if (!submitted.includes(answer)) {
       rowsMissing.push(row);
     }
     rowsAll.push(row.cloneNode(true));
@@ -28,4 +48,54 @@ async function displayAnswers() {
   answersTable.replaceChildren(...rowsAll);
 }
 
-displayAnswers();
+renderHintsAndAnswers();
+
+async function showAnswers() {
+  data = await chrome.storage.session.get("spellingBeanRevealed");
+  revealed = data.spellingBeanRevealed;
+
+  if (revealed || confirm("Are you sure you want to reveal the answers?")) {
+    const answers = document.getElementById("answers-container");
+    const hints = document.getElementById("hints-container");
+    const biggerHints = document.getElementById("bigger-hints-container");
+
+    answers.setAttribute("class", "answers shown");
+    hints.setAttribute("class", "hints hidden");
+    biggerHints.setAttribute("class", "bigger-hints hidden");
+
+    if (!revealed) {
+      await chrome.storage.session.set({
+        spellingBeanRevealed: true,
+      });
+    }
+  }
+}
+
+function showHints() {
+  const hints = document.getElementById("hints-container");
+  const biggerHints = document.getElementById("bigger-hints-container");
+  const answers = document.getElementById("answers-container");
+
+  hints.setAttribute("class", "hints shown");
+  biggerHints.setAttribute("class", "bigger-hints hidden");
+  answers.setAttribute("class", "answers hidden");
+}
+
+function showBiggerHints() {
+  const hints = document.getElementById("hints-container");
+  const biggerHints = document.getElementById("bigger-hints-container");
+  const answers = document.getElementById("answers-container");
+
+  hints.setAttribute("class", "hints hidden");
+  biggerHints.setAttribute("class", "bigger-hints shown");
+  answers.setAttribute("class", "answers hidden");
+}
+
+hintsButton = document.getElementById("hints-button");
+hintsButton.addEventListener("click", showHints);
+
+biggerHintsButton = document.getElementById("bigger-hints-button");
+biggerHintsButton.addEventListener("click", showBiggerHints);
+
+answersButton = document.getElementById("reveal-answers-button");
+answersButton.addEventListener("click", showAnswers);
