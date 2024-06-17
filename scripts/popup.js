521bd873cac633fa1238504bee9f7ca283bece85
r1yk <49421368+r1yk@users.noreply.github.com>
2024-06-17 11:22:24 -0400

    save custom ranks in storage

--- scripts/popup.js
+++ scripts/popup.js
@@ -21,22 +21,52 @@ gearIcon.addEventListener("click", async (event) => {
     });
     const editableRows = document.querySelector("#editable-ranks");
     const ranks = [];
+
     rankNames.forEach((rankName) => {
       const tableRow = document.createElement("tr");
       const normalRankCell = document.createElement("td");
       normalRankCell.innerText = rankName;
 
-      const customRankName =
-        spellingBeanRankNames[rankName.toLowerCase()] || "";
       const customRankCell = document.createElement("td");
       const customRankInput = document.createElement("input");
       customRankInput.setAttribute("type", "text");
-      customRankInput.setAttribute("value", customRankName);
+
+      const inputId = rankName.toLowerCase();
+      customRankInput.setAttribute("id", inputId);
+      customRankInput.setAttribute("name", inputId);
+
       customRankCell.append(customRankInput);
 
       tableRow.replaceChildren(normalRankCell, customRankCell);
       ranks.push(tableRow);
     });
+
     editableRows.replaceChildren(...ranks);
+
+    // Now that the inputs are created, add their default values:
+    rankNames.forEach((rankName) => {
+      const rankNameInput = document.querySelector(
+        `input#${rankName.toLowerCase()}`
+      );
+      const customRankName =
+        spellingBeanRankNames[rankName.toLowerCase()] || "";
+
+      if (customRankName) {
+        rankNameInput.setAttribute("value", customRankName);
+      }
+    });
   }
+});
+
+const ranksForm = document.getElementById("rank-names-form");
+
+ranksForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(ranksForm);
+  const rankFormData = {};
+  formData.entries().forEach((entry) => (rankFormData[entry[0]] = entry[1]));
+
+  await chrome.storage.local.set({
+    spellingBeanRankNames: rankFormData,
+  });
 });
