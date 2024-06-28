8a73951d3a4a6b560f3faa82a420acf0d5385384
Roy Kresge <49421368+r1yk@users.noreply.github.com>
2024-06-28 10:04:17 -0400

    remove tabs permissions too

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,36 +1,5 @@
 const SPELLING_BEE = "https://www.nytimes.com/puzzles/spelling-bee";
 
-// Only enable side panel when a tab turns out to be Spelling Bee:
-chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-  if (!tab.url) return;
-
-  if (tab.url.startsWith(SPELLING_BEE)) {
-    await chrome.sidePanel.setOptions({
-      tabId,
-      path: "pages/sidepanel.html",
-      enabled: true,
-    });
-  } else {
-    await chrome.sidePanel.setOptions({
-      tabId,
-      path: "pages/sidepanel.html",
-      enabled: false,
-    });
-  }
-});
-
-// Disable side panel for all other tabs
-chrome.tabs.onActivated.addListener(async ({ tabId }) => {
-  tab = await chrome.tabs.get(tabId);
-  if (tab.url && !tab.url.startsWith(SPELLING_BEE)) {
-    await chrome.sidePanel.setOptions({
-      tabId,
-      path: "pages/sidepanel.html",
-      enabled: false,
-    });
-  }
-});
-
 // Handle messages coming from the host page, including:
 //   - Clicks on the beanbag to open the side panel
 //   - Receiving the game data upon page load
